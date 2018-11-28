4d8685dd84daed188f195f875b68da564d267661
cvonsteg <cvonstegmann@gmail.com>
2018-11-28 10:13:58 +0000

    changed attributes to methods

--- main/utils/vector.py
+++ main/utils/vector.py
@@ -9,28 +9,22 @@ class Vector:
         else:
             self.vector = args
 
-        self.magnitude = self._magnitude()
-        self.mean = self._mean()
-        self.median = self._median()
-        self.variance = self._variance()
-        self.stdev = self._st_dev()
-
-    def _magnitude(self):
+    def magnitude(self):
         """Returns the scalar length of a vector"""
         return math.sqrt(sum(i**2 for i in self.vector))
 
-    def _mean(self):
+    def mean(self):
         """Calculates the mean/average of a vector of numbers"""
         return np.nanmean(self.vector)
 
-    def _median(self):
+    def median(self):
         """Calculates the central value of a vector of numbers"""
         return np.nanmedian(self.vector)
 
-    def _variance(self):
+    def variance(self):
         """Returns variance"""
         return np.nanvar(self.vector)
 
-    def _st_dev(self):
+    def st_dev(self):
         """Returns standard deviation of vector"""
         return np.nanstd(self.vector)
--- tests/test_utils/test_base_model.py
+++ tests/test_utils/test_base_model.py
@@ -7,11 +7,11 @@ class TestBaseClass(TestCase):
         self.v1 = Vector(1, 2, 3, 4, 5)
 
     def test_mean(self):
-        self.assertEqual(self.v1.mean, 3) 
+        self.assertEqual(self.v1.mean(), 3) 
 
     def test_median(self):
-        self.assertEqual(self.v1.median, 3)
+        self.assertEqual(self.v1.median(), 3)
 
     def test_magnitude(self):
         manual_mag = (1**2 + 2**2 + 3**2 + 4**2 + 5**2) ** 0.5
-        self.assertEqual(self.v1.magnitude, manual_mag)
+        self.assertEqual(self.v1.magnitude(), manual_mag)
