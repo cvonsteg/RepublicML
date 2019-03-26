65b843c603d3a121834443ffb9e739bee767c2e0
cvonsteg <cvonstegmann@gmail.com>
2019-03-26 07:18:08 +0000

    updated regression runner & added plotting to fit method

--- regression_runner.py
+++ regression_runner.py
@@ -5,9 +5,9 @@ from republic_ml.supervised_models import LinearRegression
 
 x = np.random.randint(low=0, high=20, size=100)
 y = (x * np.random.rand()) + np.random.rand()
-alpha = 0.01
+alpha = 0.001
 
 reg = LinearRegression(x, y, alpha)
 print(reg.theta)
-reg.fit(n_iter=100)
+reg.fit(n_iter=100, plot=True)
 print(reg.theta)
--- republic_ml/supervised_models/regression/linear_regression.py
+++ republic_ml/supervised_models/regression/linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from republic_ml.supervised_models.supervised_model import SupervisedModel
 
 
@@ -11,13 +12,12 @@ class LinearRegression(SupervisedModel):
         new_x = np.matrix([np.ones(len(new_x)), new_x]).T
         return self.hypothesis(new_x)
 
-    def fit(self, n_iter):
-        """Execute gradient descent algorithm to estimate theta coefficients"""
-        # Alther values of theta
-        # call cost_function() to caluclate cost
-        # TO MAKE MORE EFFICIENT: could make generator which only store minimum
-        # cost locally. but for plotting purposes this works well
-        # # TODO: WHILE COST > 0
+    def fit(self, n_iter, plot=False):
+        """
+        Conducts n_iter iterations of gradient descent steps to estimate theta.
+        Initializes a dict object with keys representing calculated cost,
+        and values representing corresponding theta coefficients.
+        """
         self.cost_dict = {}
         for i in np.arange(n_iter):
             cost = self.cost()
@@ -27,25 +27,35 @@ class LinearRegression(SupervisedModel):
 
         self.theta = self.cost_dict[min(self.cost_dict)]
 
+        if plot:
+            plt.plot(
+                [i for i in np.arange(n_iter)],
+                [v for v in self.cost_dict.keys()]
+            )
+            plt.title("Cost as a function of gradient descent iterations")
+            plt.show()
+
     def hypothesis(self, x):
         return np.dot(x, self.theta.T)
 
     def cost(self):
         """
-        Function to be minimized during regression:
-            J(theta) = 1 / 2m * SUM((h_theta(x) - y)^2)
+        Represents how large the error is between the hypothesis and labelled
+        examples.
+        Cost function (J(theta)) to be minimized during regression:
+            J(theta) = 1 / 2m * ((h_theta(x) - y) ** 2)
         """
         const = (1 / (2 * self.m))
-        square_err = np.square(self.hypothesis(self.x) - self.y)
-        cost = const * (square_err).sum()
+        square_err = np.square((self.hypothesis(self.x) - self.y))
+        cost = const * ((square_err).sum())
         return cost
 
     def _gradient_descent_step(self):
-        """Batch Gradient Descent equation to calculate theta"""
-        # h_theta = np.dot(self.theta.T, self.x)
-        # err = h_theta - self.y
-        # del_j = np.dot(err, self.x)
-        # theta_new = theta - ((alpha / m) * (del_j))
+        """
+        Batch Gradient Descent equation to calculate theta:
+            theta_i := theta_i - (alpha / m) * ((h_theta(x) - y) * x)
+
+        """
         err = self.hypothesis(self.x) - self.y
         del_j = np.dot(err.T, self.x)
         self.theta = self.theta - ((self.alpha / self.m) * del_j)
