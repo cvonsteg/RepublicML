22de4322c1b8a4cf8a0825895706cee0373e65ba
cvonsteg <cvonstegmann@gmail.com>
2019-03-26 09:25:59 +0000

    changed randomization of y var in regression runner

--- regression_runner.py
+++ regression_runner.py
@@ -4,10 +4,10 @@ import numpy as np
 from republic_ml.supervised_models import LinearRegression
 
 x = np.random.randint(low=0, high=20, size=100)
-y = (x * np.random.rand()) + np.random.rand()
+y = [i * np.random.rand() for i in x]
 alpha = 0.001
 
 reg = LinearRegression(x, y, alpha)
 print(reg.theta)
-reg.fit(n_iter=100, plot=True)
+reg.fit(n_iter=1000, plot=True)
 print(reg.theta)
