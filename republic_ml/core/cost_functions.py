a7c7719e38b582c1fd44ad434a4a812bab0604ca
cvonsteg <cvonstegmann@gmail.com>
2020-01-12 17:56:46 +0000

    beginning of redesign

---
+++ republic_ml/core/cost_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from .tensor import Tensor
+
+
+class Cost:
+    def cost(self, predicted: Tensor, actual: Tensor) -> float:
+        """ Calculate loss """
+        raise NotImplementedError
+
+
+class MSE(Cost):
+    """ Implement MeanSquaredError (MSE) """
+
+    def cost(sef, predicted: Tensor, actual: Tensor) -> float:
+        m = len(predicted)
+        return (1 / (2 * m)) * sum(np.square(predicted - actual))
---
+++ republic_ml/core/tensor.py
@@ -0,0 +1,2 @@
+""" Idea stolen from Joel Grus """
+from numpy import ndarray as Tensor
